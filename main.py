447c3c2cd94f15882d035531a3f6589c20d09824
Rıdvan Rıza Demirel <ridvan.riza.demirel@gmail.com>
2024-07-13 19:06:35 +0300

    Error fixed / new features added

--- main.py
+++ main.py
@@ -7,29 +7,35 @@ yPointList =[]
 try:
     coordTime = input("Enter coordination point :")
     coordLineColor = input("Chose coordination line color 'Red = r / Green = g / Blue = b / Cyan = c / Yellow = y' ")
-    x = 1
-    while x<=int(coordTime) :
-        xp = input("Enter x coordination :")
-        xpointList.append(int(xp))
-        x+=1
-    x = 1
-    while x<=int(coordTime) :
-        yp = input("Enter y coordination :")
-        yPointList.append(int(yp))
-        x+=1
-    xPoint = np.array(xpointList)
-    yPoint = np.array(yPointList)
+    if coordLineColor == "r" or coordLineColor=="g" or coordLineColor=="b" or coordLineColor=="c" or coordLineColor=="y":
+        x = 1
+        while x<=int(coordTime) :
+            xp = input("Enter x coordination :")
+            xpointList.append(int(xp))
+            x+=1
+        x = 1
+        while x<=int(coordTime) :
+            yp = input("Enter y coordination :")
+            yPointList.append(int(yp))
+            x+=1
+        xPoint = np.array(xpointList)
+        yPoint = np.array(yPointList)
 
-    xLabelName = input("Enter x coordination name :")
-    yLabelName = input("Enter y coordination name :")
-    graphicTitle = input("Enter graphic name :")
+        xLabelName = input("Enter x coordination name :")
+        yLabelName = input("Enter y coordination name :")
+        graphicTitle = input("Enter graphic name :")
 
-    plt.plot(xPoint,yPoint,color = f'{coordLineColor}')
+        font1 = {'family':'Times New Roman','color':'blue','size':18}
+        font2 = {'family': 'Times New Roman', 'color': 'darkred', 'size': 15}
 
-    plt.title(str(graphicTitle))
-    plt.xlabel(str(xLabelName))
-    plt.ylabel(str(yLabelName))
+        plt.plot(xPoint,yPoint,color = f'{coordLineColor}')
 
-    plt.show()
+        plt.title(str(graphicTitle),fontdict=font1)
+        plt.xlabel(str(xLabelName),fontdict=font2)
+        plt.ylabel(str(yLabelName),fontdict=font2)
+        plt.show()
+    else :
+        print("İncorrect entry ")
+
 except :
     print("Beware! Entered type is incorrect.")
